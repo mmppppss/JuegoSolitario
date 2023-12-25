5dd8b09eb2dc9fd8d606a7d4d528a6f8e9250ad1
Pedro Pozo <56263198+mmppppss@users.noreply.github.com>
2023-12-25 11:06:34 -0400

    Update JuegoSolitario.java

--- src/juegosolitario/JuegoSolitario.java
+++ src/juegosolitario/JuegoSolitario.java
@@ -6,9 +6,6 @@ package juegosolitario;
  */
 public class JuegoSolitario {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Solitario a = new Solitario();
         a.mostrarBaraja();
